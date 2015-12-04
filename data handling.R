bb9cdac014ae2aec0a00196e8a1dda6a3100d0e6
Matthias 'Theo' Theobald <m.theo@arcor.de>
2015-12-04 15:10:09 +0100

    change read to current directory

--- data handling.R
+++ data handling.R
@@ -1,6 +1,5 @@
-db_chamois1 <- read.csv("~/Studium/R Projects/topic 5 - chamois horn/chamois1.csv", sep=";")
-
-db_chamois2 <- read.csv("~/Studium/R Projects/topic 5 - chamois horn/chamois2.csv", sep=";")
+db_chamois1 <- read.csv("chamois1.csv", sep=";")
+db_chamois2 <- read.csv("chamois2.csv", sep=";")
 
 db <- merge(db_chamois1, db_chamois2, all.x=T)
 
@@ -15,4 +14,3 @@ outersect(colnames(db_chamois1), colnames(db_chamois2))
 
 # where are the data holes
 
-with(db_chamois2, tapply(twinter.m)
